6c0e617b891ad7b7e1e5666192071a22043ef5bb
Justin Bonnici <justin-edward.bonnici@gov.mt>
2022-02-23 13:38:38 +0100

    Initital commit. Project files from: https://nxhoaf.wordpress.com/2014/08/03/spring-mvc-creating-a-project-from-scratch-using-maven/

---
+++ src/main/java/com/gmail/nxhoaf/HelloWorldController.java
@@ -0,0 +1,22 @@
+package com.gmail.nxhoaf;
+
+import java.util.Date;
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloWorldController {
+	// Project guide here: https://nxhoaf.wordpress.com/2014/08/03/spring-mvc-creating-a-project-from-scratch-using-maven/
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		Date date = new Date();
+		model.addAttribute("serverTime", date);
+		model.addAttribute("happyMessage", "Good things are coming");
+		return "helloWorld";
+	}
+}
---
+++ src/main/webapp/WEB-INF/views/helloWorld.jsp
@@ -0,0 +1,8 @@
+<%@ page isELIgnored="false" %>
+ 
+<html>
+<body>
+<h2>Hello World at: ${serverTime}</h2>
+<h3>Id like to say: ${happyMessage}</h3>
+</body>
+</html>
