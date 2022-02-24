cc5dc24a69198477826c7e1b9d3523b9bc977917
Justin Bonnici <justin-edward.bonnici@gov.mt>
2022-02-24 13:22:55 +0100

    Add link to Baeldung Spring MVC article

--- src/main/java/com/gmail/nxhoaf/HelloWorldController.java
+++ src/main/java/com/gmail/nxhoaf/HelloWorldController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HelloWorldController {
 	// Project guide here: https://nxhoaf.wordpress.com/2014/08/03/spring-mvc-creating-a-project-from-scratch-using-maven/
+	// Good diagram to understand Spring MVC architecture here: https://www.baeldung.com/spring-controllers
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
